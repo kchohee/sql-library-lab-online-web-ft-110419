58887620a65bd6f15bcdc862194e6b4723672862
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-14 07:14:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,19 +1,19 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year 
-  FROM books 
+  "SELECT books.title, books.year
+  FROM books
   WHERE series_id = 1 ORDER BY books.year ASC;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto 
-  FROM characters 
+  "SELECT characters.name, characters.motto
+  FROM characters
   ORDER BY LENGTH(characters.motto) DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.id) 
-  FROM characters 
+  "SELECT characters.species, COUNT(characters.id)
+  FROM characters
   GROUP BY characters.species ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
