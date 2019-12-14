770837622a0f9f5205016a62caade2ef9f335889
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-14 07:05:23 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,8 +5,7 @@ INSERT INTO books (title, year, series_id) VALUES
 ('title 1', 2020, 1), ('title 2', 2019, 2), ('title 3', 2018, 1), ('title 3', 2017, 1), ('title 4', 2016, 2)('title 5', 2016, 1)('title 6', 2016, 2);
 
 INSERT INTO characters (name, species, motto, author_id) VALUES ('name 1', 'species 1', 'motto 1', 1), ('name 2', 'species 2', 'motto 2', 2), ('name 3', 'species 3', 'motto 3', 1), ('name 4', 'species 4', 'motto 4', 2), ('name 5', 'species 5', 'motto 5', 1), ('name 6', 'species 6', 'motto 6', 2),
- ('name 7', 'species 7', ' motto 7', 1);
-
+ ('name 7', 'species 7', ' motto 7', 1), ('name 8', 'species 8', ' motto 8', 2);
 INSERT INTO subgenres (name) VALUES
 ('sub gen'), ('sub gen again');
 
@@ -14,4 +13,4 @@ INSERT INTO authors (name) VALUES
 ('dude'), ('bro');
 
 INSERT INTO character_books (book_id, character_id) VALUES
-(1, 1), (2, 2), (3, 3), (4, 4), (1, 5), (2, 6), (3, 7), (4, 8), (1, 1), (2, 2), (3, 3), (4, 4), (1, 5), (2, 6), (3, 7);
+(1, 1), (2, 2), (3, 3), (4, 4), (1, 5), (2, 6), (3, 7), (4, 8), (1, 1), (2, 2), (3, 3), (4, 4), (1, 5), (2, 6), (3, 7), (4, 8);
