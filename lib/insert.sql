3893571b5ab8f83e0caaf5ed52c28dc4ac1b8f9d
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-14 07:04:23 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES
 ('Book series 1', 1, 1), ('Book series 2', 2, 2);
 
 INSERT INTO books (title, year, series_id) VALUES
-('title 1', 2020, 1), ('title 2', 2019, 2), ('title 3', 2018, 1), ('title 3', 2017, 1), ('title 4', 2016, 2);
+('title 1', 2020, 1), ('title 2', 2019, 2), ('title 3', 2018, 1), ('title 3', 2017, 1), ('title 4', 2016, 2)('title 5', 2016, 1)('title 6', 2016, 2);
 
 INSERT INTO characters (name, species, motto, author_id) VALUES ('name 1', 'species 1', 'motto 1', 1), ('name 2', 'species 2', 'motto 2', 2), ('name 3', 'species 3', 'motto 3', 1), ('name 4', 'species 4', 'motto 4', 2), ('name 5', 'species 5', 'motto 5', 1), ('name 6', 'species 6', 'motto 6', 2),
  ('name 7', 'species 7', ' motto 7', 1);
